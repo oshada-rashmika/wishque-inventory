2d6ea7f1057b3bdcc0e7c49e6a0b1403b7089928
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-06-22 23:24:26 +0530

    eat: configure minimalist responsive layout wrapper container

--- wishque-ims/src/app/dashboard/layout.tsx
+++ wishque-ims/src/app/dashboard/layout.tsx
@@ -38,7 +38,7 @@ export default async function DashboardLayout({ children }: { children: React.Re
   return (
     <div className="min-h-screen bg-background flex flex-col">
       <Navbar profile={profile} />
-      <main className="flex-1 p-6">
+      <main className="flex-1 w-full max-w-7xl mx-auto p-4 md:p-6">
         {children}
       </main>
     </div>
